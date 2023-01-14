a79a9a1b84fcfaacc5ef15edffeb16fd55e9ea2f
Andrey Mironenko <qwerty.mironenko@gmail.com>
2023-01-15 01:34:33 +0300

    rename

--- simple_broker_example/publisher.py
+++ simple_broker_example/publisher.py
@@ -6,8 +6,8 @@ r = redis.Redis(host='localhost', port=6379, db=0)
 listname = 'my-list-1'
 basemessage = 'my data'
 # r.publish('my-channel-1', 'my data')
-# 
-if sys.argv[1].isdecimal():
+
+if len(sys.argv)>1 and sys.argv[1].isdecimal():
     print(sys.argv[1])
     for i in range(int(sys.argv[1])):
         message = basemessage +'-'+str(i)
@@ -16,4 +16,5 @@ if sys.argv[1].isdecimal():
         r.rpush(listname, message)
         print(f'send "{message}" to "{listname}"')
 else:
+    r.rpush(listname, basemessage)
     print(f'send "{basemessage}" to "{listname}"')
